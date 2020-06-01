bf3ca722ac05d5d513a66b3c4e00a23ea8ee3dbe
Ilya Shevchenko <ilya.93@hotmail.com>
2020-06-01 22:25:59 +0100

    added requirements.txt

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,10 @@
 function selectcapture(gallery_thumbnail){
     console.log(`Selecting capture: ${gallery_thumbnail}`);
+    $('.selected-thumbnail').addClass('deselected-thumbnail');
+    $('.selected-thumbnail').removeClass('selected-thumbnail');
     thumbnail_image = $(gallery_thumbnail).find('.thumbnail-image');
     thumbnail_image.removeClass('deselected-thumbnail');
     thumbnail_image.addClass('selected-thumbnail');
+    thumbnail_src = thumbnail_image.attr('src');
+    $('#selected-capture').attr('src',thumbnail_src)
 }
